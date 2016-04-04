6c3458136edc0f88e2877fede6432b1b30d15650
Useless-Sadist <spammedspamspamspam@gmail.com>
2016-04-04 08:19:53 -0500

    Worked Some

---
+++ engine.js
@@ -0,0 +1,17 @@
+get = function(id) {return document.getElementById(id)}
+var Trait = {
+	create: function(num) {
+		get("1trait").style.visibility = "hidden"
+		get("2trait").style.visibility = "hidden"
+		for(i in num){
+			get()
+		}
+	},
+	parents: function() {
+		
+	},
+	cross: function() {
+		
+	}
+}
+var t = Trait
