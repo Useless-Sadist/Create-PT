02227f747f3e2ff10a24cb1e68e22d438d3bca86
Useless-Sadist <spammedspamspamspam@gmail.com>
2016-04-05 09:30:15 -0500

    Did more work

--- engine.js
+++ engine.js
@@ -1,11 +1,18 @@
 get = function(id) {return document.getElementById(id)}
+
 var Trait = {
 	create: function(num) {
 		get("1trait").style.visibility = "hidden"
 		get("2trait").style.visibility = "hidden"
-		for(i in num){
-			get()
+		console.log(num)
+		for(i = 0;i < num;i++){
+			get("intArea").innerHTML += "<br> Trait " + (i+1) + "<br> <input type='radio' name='incom-coDom" + (i+1) + "' id='normDom" + (i+1) + "'>Normal Dominance</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='incomDom" + (i+1) + "'>Incompletely Dominant</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='coDom" + (i+1) + "'>Co-Dominant</input>"
+			get("intArea").innerHTML += "<br> <input type='checkbox' id='sex-link" + (i+1) + "'>Sex-Linked</input> <br> <input type='checkbox' id='multAlleles'" + (i+1) + "'>Multiple Alleles</input><br>"
 		}
+		get("intArea").innerHTML += "<button onclick='t.specify(num)' value='Confirm'>"
+	},
+	specify: function(traitNum) {
+		
 	},
 	parents: function() {
 		
