15d6952468747fece9f2dafc9cc8865191c605d7
Useless-Sadist <spammedspamspamspam@gmail.com>
2016-04-06 08:19:59 -0500

    More Working
    Running into problems re radio buttons and checkboxes; HOW

--- engine.js
+++ engine.js
@@ -1,18 +1,26 @@
 get = function(id) {return document.getElementById(id)}
-
+var multAllelesIndic = [0,0]
+var coDomIndic = [0,0]
+var incomDomIndic = [0,0]
+var sexLinkIndic = [0,0]
 var Trait = {
 	create: function(num) {
 		get("1trait").style.visibility = "hidden"
 		get("2trait").style.visibility = "hidden"
-		console.log(num)
+		get("instructions").innerHTML = "Now, select the specific attributes of your trait(s)"
 		for(i = 0;i < num;i++){
-			get("intArea").innerHTML += "<br> Trait " + (i+1) + "<br> <input type='radio' name='incom-coDom" + (i+1) + "' id='normDom" + (i+1) + "'>Normal Dominance</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='incomDom" + (i+1) + "'>Incompletely Dominant</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='coDom" + (i+1) + "'>Co-Dominant</input>"
-			get("intArea").innerHTML += "<br> <input type='checkbox' id='sex-link" + (i+1) + "'>Sex-Linked</input> <br> <input type='checkbox' id='multAlleles'" + (i+1) + "'>Multiple Alleles</input><br>"
+			get("crtArea").innerHTML += "<br> Trait " + (i+1) + "<br> <input type='radio' name='incom-coDom" + (i+1) + "' id='normDom" + (i+1) + "'>Normal Dominance</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='incomDom" + (i+1) + "'>Incompletely Dominant</input> <input type='radio' name='incom-coDom" + (i+1) + "' id='coDom" + (i+1) + "'>Co-Dominant</input>"
+			get("crtArea").innerHTML += "<br> <input type='checkbox' id='sex-link" + (i+1) + "'>Sex-Linked</input> <br> <input type='checkbox' id='multAlleles'" + (i+1) + "'>Multiple Alleles</input><br>"
 		}
-		get("intArea").innerHTML += "<button onclick='t.specify(num)' value='Confirm'>"
+		get("crtArea").innerHTML += "<br><button onclick='t.assign(" + num + ")'>Confirm</button>"
+	},
+	assign: function(num){
+		for(i=0,i<num,i++){
+			
+		}
 	},
 	specify: function(traitNum) {
-		
+		get("crtArea").style.visibility = "hidden"
 	},
 	parents: function() {
 		
